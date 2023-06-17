f07bd8f4114d4d22665ac3f434724a6ec716d6b3
Lennart Molnar <pernicius@web.de>
2023-06-17 20:09:09 +0200

    Added initial setup script for Windows

---
+++ scripts/windows/setup.bat
@@ -0,0 +1,21 @@
+@echo off
+
+
+echo ===== Downloading Vulkan SDK...
+powershell -Command "Invoke-WebRequest https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe -OutFile vulkan-sdk.exe"
+pause
+
+echo ===== Installing Vulkan SDK...
+call vulkan-sdk.exe
+echo Install Vulkan SDK...
+pause
+
+
+rem echo ===== Generating Visual Studio 2022 workspace...
+rem pushd ..\..\
+rem call vendor\premake\windows\premake5.exe vs2022
+rem popd
+
+
+echo ===== Done.
+pause
